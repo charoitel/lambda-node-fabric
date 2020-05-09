486e9ec80d130098909e31b8ce51fb9f96d08982
charoite <charoitel@icloud.com>
2020-05-09 17:40:13 +0800

    Initial commit

---
+++ function/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const fabric = require('fabric').fabric;
+
+exports.handler = async (event) => {
+    var canvas = new fabric.Canvas(null, {
+        width: 200,
+        height: 200,
+        backgroundColor: 'navy'
+    });
+
+    var rect = new fabric.Rect({
+        width: 100,
+        height: 100,
+        top: 0,
+        left: 0,
+        fill: 'orange'
+    });
+
+    canvas.add(rect);
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'image/png'
+        },
+        body: canvas.toDataURL().replace(/data:image\/png;base64,/, ''),
+        isBase64Encoded: true
+    }
+}
